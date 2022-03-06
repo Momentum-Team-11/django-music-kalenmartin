41dc5fdfe38378960bcc966c0034fff9e52e0f66
Kalen Martin <kalen.martin1@gmail.com>
2022-03-06 10:34:53 -0500

    still working, only title displaying however

--- albums/forms.py
+++ albums/forms.py
@@ -1,13 +1,15 @@
-from socket import fromshare
+# from socket import fromshare
 from django import forms
 from .models import Album
 
 class AlbumForm(forms.ModelForm):
     class Meta:
         model = Album
-        field = ['title']
-        field = ['artist']
-        field = ['created_at']
+        fields = [
+            'title',
+            'artist',
+            'created_at',
+        ]
 
 # class ArtistForm(forms.ModelForm):
 #     class Meta:
--- django_music/urls.py
+++ django_music/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.conf import settings
+from django.urls import path, include
 from albums import views as album_views
 
 urlpatterns = [
